b97f6cd6ba4af8a4cbc82d0ed7c0a58127271451
Monks <nephast@stephans-MacBook-Pro.local>
2016-01-12 16:50:32 +0000

    first test

---
+++ app.js
@@ -0,0 +1,102 @@
+/*
+function mapForEach(arr, fn) {
+    
+    var newArr = [];
+    for (var i=0; i < arr.length; i++) {
+        newArr.push(
+            fn(arr[i])   
+        )
+    };
+    
+    return newArr;
+}
+
+var arr1 = ['0145678765','0176543456','0145678334'];
+console.log( arr1);
+
+
+var arr2 = mapForEach(arr1, function(item) {
+   return item + ' ' + 'traduction ici'; 
+});
+console.log(arr2);
+
+var reponse = 'bleu'
+
+var arr3 = mapForEach(arr1, function(item) {
+   return item === reponse; 
+});
+console.log(arr3);
+
+
+var checkPastLimit = function(limiter, item) {
+    return item > limiter;   
+}
+var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
+console.log(arr4);
+
+
+var addDesignator= function(designator) {
+    return function(designator, item) {
+        if (designator === 33) {
+             return 'POUR LA FRANCE COMPOSEZ ' + designator + ' ' + item;   
+        } else
+        return 'AUTRE PAYS' + ' ' + item;   
+    }.bind(this, designator); 
+};
+
+var arr5 = mapForEach(arr1, addDesignator(33));
+console.log(arr5);
+*/
+
+
+
+var secret = ['bleu', 'rouge', 'vert', 'jaune'];
+console.log('secret is: ' + secret);
+
+var guess1 = ['jaune','vert','rouge','bleu'];
+console.log('guess is : ' + guess1)
+
+function matchArrays(arr, fn) {
+    
+    var newArray = [];
+    for (i= 0; i <secret.length; i++){
+        newArray.push(
+        func(arr[i]))
+    };
+    return newArray;
+}
+
+var answer1 = matchArray(secret, function(item) {
+    if secret[0]===guess1[0]
+   return item + ' ' + 'traduction ici'; 
+});
+console.log(reponse1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
